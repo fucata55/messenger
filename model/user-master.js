e150c96bd7164ff6720fa74ad43f574e9d14f2db
fucata55 <sliong325@gmail.com>
2020-03-05 22:00:17 -0600

    add get method

--- model/user-master.js
+++ model/user-master.js
@@ -4,25 +4,33 @@ const uniqid = require('uniqid')
 const isStrWithLength = require('../util/library').isStrWithLength
 
 class UserMaster {
-	constructor() {
-		this.users = []
-	}
+  constructor() {
+    this.users = []
+  }
 
-	create(name) {
-		let result = null
-		if (isStrWithLength(name)) {
-			const id = uniqid('user')
-			const param = {
-				id,
-				name
-			}
-			result = new User(param)
-			this.users.push(result)
-		}
-		return result
-	}
+  create(name) {
+    let result = null
+    const validInput = isStrWithLength(name)
+    if (validInput) {
+      const id = uniqid('user')
+      const param = {
+        id,
+        name
+      }
+      result = new User(param)
+      this.users.push(result)
+    }
+    return result
+  }
 
-	get() {}
+  get(userId) {
+    let result
+    const validInput = isStrWithLength(userId)
+    if (validInput) {
+      result = this.users.find((user) => user.id === userId)
+    }
+    return result || null
+  }
 }
 
 module.exports = UserMaster
--- model/user-master.test.js
+++ model/user-master.test.js
@@ -7,32 +7,68 @@ const assert = chai.assert
 const isStrWithLength = require('../util/library').isStrWithLength
 
 describe('UserMaster', () => {
-	let userMaster
+  let userMaster
 
-	beforeEach(() => {
-		userMaster = new UserMaster()
-	})
-	describe('contructor', () => {
-		it('should instantiate UserMaster', () => {
-			assert.isArray(userMaster.users, 'userMaster.users should be instantiated as an array')
-		})
-	})
-	describe('create', () => {
-		it('should return a User', () => {
-			const name = 'Natalie'
-			const result = userMaster.create(name)
-			assert.deepEqual(result.name, name, 'parameter name should be passed along to User')
-			assert.deepEqual(isStrWithLength(result.id), true, 'should generate id for User')
-		})
+  beforeEach(() => {
+    userMaster = new UserMaster()
+  })
+  describe('contructor', () => {
+    it('should instantiate UserMaster', () => {
+      assert.isArray(
+        userMaster.users,
+        'userMaster.users should be instantiated as an array'
+      )
+    })
+  })
+  describe('create', () => {
+    it('should return a User', () => {
+      const name = 'Natalie'
+      const result = userMaster.create(name)
+      assert.deepEqual(
+        result.name,
+        name,
+        'parameter name should be passed along to User'
+      )
+      assert.deepEqual(
+        isStrWithLength(result.id),
+        true,
+        'should generate id for User'
+      )
+    })
 
-		it('should add user to users list', () => {
-			const users = [...userMaster.users]
-			userMaster.create('Natalie')
-			assert.deepEqual(userMaster.users.length > users.length, true, 'should add user to users list')
-		})
-		it('should require name parameter', () => {
-			const result = userMaster.create()
-			assert.isNull(result, 'should not return a User')
-		})
-	})
+    it('should add user to users list', () => {
+      const users = [...userMaster.users]
+      userMaster.create('Natalie')
+      assert.deepEqual(
+        userMaster.users.length > users.length,
+        true,
+        'should add user to users list'
+      )
+    })
+    it('should require name parameter', () => {
+      const result = userMaster.create()
+      assert.isNull(result, 'should not return a User')
+    })
+  })
+
+  describe('get', () => {
+    let user
+    let userId
+
+    beforeEach(() => {
+      user = userMaster.create('Natalie')
+      userId = user.id
+    })
+
+    it('should return a user', () => {
+      const result = userMaster.get(userId)
+      assert.instanceOf(result, User, 'result should be User type')
+    })
+
+    it('should return null', () => {
+      const wrongUserId = userId + '2'
+      const result = userMaster.get(wrongUserId)
+      assert.isNull(result, 'result should be null')
+    })
+  })
 })
